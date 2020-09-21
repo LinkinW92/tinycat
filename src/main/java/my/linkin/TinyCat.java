a9b445a7758fb794d592c1e0864bb8daac4f24b9
linkin <2772646143@qq.com>
2020-09-22 01:12:13 +0800

    Initial commit

---
+++ src/main/java/my/linkin/TinyCat.java
@@ -0,0 +1,14 @@
+package my.linkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world! i am tiny cat
+ */
+@SpringBootApplication
+public class TinyCat {
+    public static void main(String[] args) {
+        SpringApplication.run(TinyCat.class, args);
+    }
+}
---
+++ src/test/java/my/linkin/TinyCatTest.java
@@ -0,0 +1,20 @@
+package my.linkin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple TinyCat.
+ */
+public class TinyCatTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
