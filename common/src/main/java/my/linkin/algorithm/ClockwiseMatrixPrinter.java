eafcf9f102e32b2b19d65d746e63ae56225e8dc2
chunhui.wu <2772646143@qq.com>
2020-09-29 19:36:23 +0800

    add algorithm package

--- common/src/main/java/my/linkin/algorithm/ClockwiseMatrixPrinter.java
+++ common/src/main/java/my/linkin/algorithm/ClockwiseMatrixPrinter.java
@@ -7,33 +7,43 @@ package my.linkin.algorithm;
  */
 public class ClockwiseMatrixPrinter {
 
-    public static void clockwise(int[][] a) {
+    public int[] spiralOrder(int[][] a) {
+        if (a.length == 0) {
+            return new int[0];
+        }
+        int len = a.length * a[0].length;
+        int[] r = new int[len];
         final int MAX_X = a.length, MAX_Y = a[0].length;
-        int ymin = 0, ymax = MAX_Y - 1, xmin = 0, xmax = MAX_X - 1;
+        int ymin = 0, ymax = MAX_Y - 1, xmin = 0, xmax = MAX_X - 1, idx = 0;
         while (xmin < xmax && ymin < ymax) {
             for (int i = ymin; i <= ymax; i++) {
                 sout(a[xmin][i]);
+                r[idx++] = (a[xmin][i]);
             }
             for (int j = xmin + 1; j <= xmax; j++) {
                 sout(a[j][ymax]);
+                r[idx++] = (a[j][ymax]);
             }
             for (int m = ymax - 1; m >= 0; m--) {
                 sout(a[xmax][m]);
+                r[idx++] = (a[xmax][m]);
             }
             for (int n = xmax - 1; n > xmin; n--) {
                 sout(a[n][ymin]);
+                r[idx++] = (a[n][ymin]);
             }
             xmin++;
             ymin++;
             xmax--;
             ymax--;
         }
-        while (xmin < xmax) {
-            sout(a[xmin++][ymin]);
+        while (idx < len && xmin <= xmax) {
+            r[idx++] = (a[xmin++][ymin]);
         }
-        while (ymin <= ymax) {
-            sout(a[xmin][ymin++]);
+        while (idx < len && ymin <= ymax) {
+            r[idx++] = (a[xmin][ymin++]);
         }
+        return r;
     }
 
     public static void sout(int a) {
@@ -41,7 +51,10 @@ public class ClockwiseMatrixPrinter {
     }
 
     public static void main(String[] args) {
-        int[][] a = new int[][]{{1, 2, 3, 4, 100}, {5, 6, 7, 8, 200}, {9, 10, 11, 12, 300}};
-        clockwise(a);
+        int[][] a = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[] r = new ClockwiseMatrixPrinter().spiralOrder(a);
+//        for (int i : r) {
+//            System.out.print(i + " ");
+//        }
     }
 }
