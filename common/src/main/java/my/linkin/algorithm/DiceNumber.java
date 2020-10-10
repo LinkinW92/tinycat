7a899839ee06ff84b240cb315abec7f227ac8c8a
chunhui.wu <2772646143@qq.com>
2020-10-10 19:04:10 +0800

    promote dice number algorithm

--- common/src/main/java/my/linkin/algorithm/DiceNumber.java
+++ common/src/main/java/my/linkin/algorithm/DiceNumber.java
@@ -1,6 +1,7 @@
 package my.linkin.algorithm;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author chunhui.wu
@@ -13,46 +14,61 @@ public class DiceNumber {
      * @return 各个骰子的点数之和对应的概率，点数之和由小到大排列
      */
     public double[] twoSum(int n) {
-        Map<Integer, Integer> m1 = new HashMap<>(128), m2 = new HashMap<>(128), temp;
-        for (int i = 1; i <= 6; i++) {
+        int pn = n, k, d = 6, total = (int) Math.pow(6d, pn);
+        Map<Integer, Integer> m1 = new HashMap<>(32), m2;
+        for (int i = 1; i <= d; i++) {
             m1.put(i, 1);
         }
-        final int pn = n;
         while (--n > 0) {
+            m2 = new HashMap<>(32);
             for (Map.Entry<Integer, Integer> entry : m1.entrySet()) {
-                for (int i = 1; i <= 6; i++) {
-                    int k = entry.getKey() + i;
-                    if (m2.containsKey(k)) {
-                        m2.put(k, m2.get(k) + 1);
-                    } else {
-                        m2.put(k, 1);
-                    }
+                for (int i = 1; i <= d; i++) {
+                    k = entry.getKey() + i;
+                    m2.put(k, m2.getOrDefault(k, 0) + entry.getValue());
                 }
             }
-            temp = m1;
+            m2.remove(pn - n);
             m1 = m2;
-            m2 = temp;
         }
-        List<Integer> keys = new ArrayList<>(m1.keySet());
-        Collections.sort(keys, Comparator.comparing(Integer::intValue));
         double[] r = new double[5 * pn + 1];
-        int idx = 0;
-        double total = Math.pow(6d, pn);
-        for (Integer key : keys) {
-            if (key < pn) {
-                continue;
-            }
-            r[idx++] = (double) m1.get(key) / total;
-        }
-        for (double d : r) {
-            System.out.println(d);
+        for (Integer key : m1.keySet()) {
+            r[key - pn] = (double) m1.get(key) / total;
         }
         return r;
     }
 
+    public double[] twoSumAdvanced(int n) {
+        int dp[] = new int[70];
+        for (int i = 1; i <= 6; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 6 * i; j >= i; j--) {
+                dp[j] = 0;
+                for (int cur = 1; cur <= 6; cur++) {
+                    if (j - cur < i - 1) {
+                        break;
+                    }
+                    dp[j] += dp[j - cur];
+                }
+
+            }
+        }
+        double all = Math.pow(6, n);
+        double[] res = new double[5 * n + 1];
+        for (int i = n; i <= 6 * n; i++) {
+            res[i - n] = (dp[i] * 1.0 / all);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         DiceNumber dn = new DiceNumber();
-        dn.twoSum(3);
+        int n = 11;
+        double[] r1 = dn.twoSum(n), r2 = dn.twoSumAdvanced(n);
+        for (int i = 0; i <= 5 * n; i++) {
+            System.out.println(r1[i] + " || " + r2[i]);
+        }
         //[0.00463,0.01389,0.02778,0.0463,0.06944,0.09722,0.11574,0.125,0.125,0.11574,0.09722,0.06944,0.0463,0.02778,0.01389,0.00463]
     }
 }
