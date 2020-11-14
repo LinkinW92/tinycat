f1b2870586095d6700b5add06596ba2e517d810a
qingwei@xiaohongshu.com <linkin@qingweixiaohongshucomdeMacBook-Pro.local>
2020-11-14 16:31:29 +0800

    make a try

--- common/src/main/java/my/linkin/algorithm/No1Counter.java
+++ common/src/main/java/my/linkin/algorithm/No1Counter.java
@@ -13,29 +13,38 @@ public class No1Counter {
      * counter logic
      *
      * @param n the given number
-     * @return digit one count
+     * @return count of digit one
      */
     public int countDigitOne(int n) {
         int counter = 0;
-        int max = String.valueOf(n).length();
+        String sn = String.valueOf(n);
+        int max = sn.length();
+        final int m = max;
         while (max > 0) {
             // 100, 010, 001
             // 110, 101, 011,
             // 111
-            for (int i = 1; i <= max; i++) {
-                counter += (factorial(max) / factorial(i) / factorial(max - i));
+            // 800000000 ~ 87654321  -> 0 ~ 7654321
+            if (max < m) {
+                for (int i = 1; i <= max; i++) {
+                    counter = counter + (int) Math.pow(10d, max - i) - (int) Math.pow(10d, max - i - 1);
+                }
+            } else {
+                if (sn.charAt(0) == '1') {
+                    int c = 1;
+                    for (int i = 1; i < m; i++) {
+                        c *= (sn.charAt(i) + 1);
+                    }
+                    counter += c;
+                }
+                String idx = String.valueOf(n - (int) Math.pow(10, m));
+                for (int i = 1; i <= max; i++) {
+                    max--;
+                }
             }
             max--;
         }
         return counter;
-    }
-
-    private int factorial(int n) {
-        int r = 1;
-        for (int i = 2; i <= n; i++) {
-            r *= i;
-        }
-        return r;
     }
 
     public static void main(String[] args) {
